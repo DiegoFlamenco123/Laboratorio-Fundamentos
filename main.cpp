330c8d6d18710009518639fa42241e3f0cdd651e
unknown <00221020@uca.edu.sv>
2020-05-05 16:50:01 -0600

    hola

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main () {
+
+string nombre;
+string nombre2;
+string apellido;
+string apellido2;
+
+cout<<"ingrese su nombre y apellido"<<endl;
+cin>>nombre>>nombre2>>
+}
